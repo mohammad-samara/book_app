4fa9c8d82f5abf2523bbc6b1f5c2354e61035329
mohammad samara <mohammad.ab.samara@gmail.com>
2020-08-09 13:30:32 +0300

    part1

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ app.use(express.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 //const pg = require('pg');  // sql first step in intializing
 const superagent = require('superagent');
+const { response } = require('express');
 //const client = new pg.Client(process.env.DATABASE_URL);
+
+//routes
+app.get('/', (req, res) => {
+res.render('pages/index');
+})
+
+app.get('/hello', (req, res) => {
+    res.render('pages/index');
+    })
+
 
 app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`);
